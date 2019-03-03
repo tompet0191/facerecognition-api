4599443eece2de0ff4125f2e6df72a175e2cd912
Tommy Pettersson <tommypet@kth.se>
2019-03-03 20:54:27 +0100

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-globular-60918',
       user : 'Test',
       password : 'test',
       database : 'facerecognition'
